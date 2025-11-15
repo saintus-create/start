84cfa97bccc821b62abfa172e65cbcdfacc22f67
rosesareblack <kafka_esque@icloud.com>
2025-11-14 20:43:31 -0800

    Remove Stack Auth system and related dependencies

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,26 +1,10 @@
 import { FeatureGrid } from "@/components/features";
 import { Hero } from "@/components/hero";
 import { PricingGrid } from "@/components/pricing";
-import { stackServerApp } from "@/stack";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { ComponentIcon, Users } from "lucide-react";
 
-export default async function IndexPage() {
-  const project = await stackServerApp.getProject();
-  if (!project.config.clientTeamCreationEnabled) {
-    return (
-      <div className="w-full min-h-96 flex items-center justify-center">
-        <div className="max-w-xl gap-4">
-          <p className="font-bold text-xl">Setup Required</p>
-          <p className="">
-            {
-              "To start using this project, please enable client-side team creation in the Stack Auth dashboard (Project > Team Settings). This message will disappear once the feature is enabled."
-            }
-          </p>
-        </div>
-      </div>
-    );
-  }
+export default function IndexPage() {
 
   return (
     <>
